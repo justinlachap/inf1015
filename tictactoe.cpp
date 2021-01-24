649a05b93c22ddf9307c3f0aaacce0129366c886
Justin Lachapelle <46169956+justinlachap@users.noreply.github.com>
2021-01-24 16:37:41 -0500

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -4,10 +4,20 @@
 
 using namespace std;
 
+string joueur(char symbol) // détermine quel joueur a gagné
+{
+	if (symbol == 'x') {
+		return "Le joueur 1 gagne";
+	}
+	else if (symbol == 'o'){
+		return "Le joueur 2 gagne";
+		}
+}
+
 int main()
 {
 	string grilleChoisie, jeu, output;
-	char listeJeu[3][3];
+	char listeJeu[3][3]; // tableau tic-tac-toe
 	int indexLigne = 0;
 	cout << "Entrez la grille de votre choix: ";
 	cin >> grilleChoisie;
@@ -23,38 +33,18 @@ int main()
 		fichier.close();
 		indexLigne = 0;
 	}
-	// gagner horizontalement
 	for (int i = 0; i < 3; i++) {
-		if (listeJeu[i][0] == listeJeu[i][1] and listeJeu[i][0] == listeJeu[i][2]) {
-			if (listeJeu[i][0] == 'x') {
-
-				output = "Le joueur 1 gagne";
-			}
-			else if (listeJeu[i][0] == 'o') {
-
-				output = "Le joueur 2 gagne";
-			}
+		if (listeJeu[i][0] == listeJeu[i][1] and listeJeu[i][0] == listeJeu[i][2]) { // gagner horizontalement
+			output = joueur(listeJeu[i][0]);
 		}
-	}
-	// gagner verticalement
-	for (int i = 0; i < 3; i++) {
-		if (listeJeu[0][i] == listeJeu[1][i] and listeJeu[0][i] == listeJeu[2][i]) {
-			if (listeJeu[0][i] == 'x') {
-				output = "Le joueur 1 gagne";
-			}
-			else if (listeJeu[0][i] == 'o') {
-				output = "Le joueur 2 gagne";
-			}
+
+		if (listeJeu[0][i] == listeJeu[1][i] and listeJeu[0][i] == listeJeu[2][i]) { // gagner verticalement
+			output = joueur(listeJeu[0][i]);
 		}
 	}
 	// gagner en diagonale 
 	if ((listeJeu[0][0] == listeJeu[1][1] and listeJeu[0][0] == listeJeu[2][2]) or (listeJeu[0][2] == listeJeu[1][1] and listeJeu[0][2] == listeJeu[2][0])) {
-		if (listeJeu[1][1] == 'x') {
-			output = "Le joueur 1 gagne";
-		}
-		else if (listeJeu[1][1] == 'o') {
-			output = "Le joueur 2 gagne";
-		}
+		output = joueur(listeJeu[1][1]);
 
 	}
 	if (output.length() == 0) {
