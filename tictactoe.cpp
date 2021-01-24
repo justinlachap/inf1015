50516fa3b68a53fa3db43127158421a177ca9c17
Justin Lachapelle <46169956+justinlachap@users.noreply.github.com>
2021-01-24 13:56:38 -0500

    terminé

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 int main()
@@ -24,18 +25,20 @@ int main()
 	}
 	// gagner horizontalement
 	for (int i = 0; i < 3; i++) {
-		if (listeJeu[i][0] == listeJeu[i][1] == listeJeu[i][2]) {
+		if (listeJeu[i][0] == listeJeu[i][1] and listeJeu[i][0] == listeJeu[i][2]) {
 			if (listeJeu[i][0] == 'x') {
+
 				output = "Le joueur 1 gagne";
 			}
 			else if (listeJeu[i][0] == 'o') {
+
 				output = "Le joueur 2 gagne";
 			}
 		}
 	}
 	// gagner verticalement
 	for (int i = 0; i < 3; i++) {
-		if (listeJeu[0][i] == listeJeu[1][i] == listeJeu[2][i]) {
+		if (listeJeu[0][i] == listeJeu[1][i] and listeJeu[0][i] == listeJeu[2][i]) {
 			if (listeJeu[0][i] == 'x') {
 				output = "Le joueur 1 gagne";
 			}
@@ -45,16 +48,18 @@ int main()
 		}
 	}
 	// gagner en diagonale 
-	if ((listeJeu[0][0] == listeJeu[1][1] == listeJeu[2][2]) or (listeJeu[0][2] == listeJeu[1][1] == listeJeu[2][0])) {
+	if ((listeJeu[0][0] == listeJeu[1][1] and listeJeu[0][0] == listeJeu[2][2]) or (listeJeu[0][2] == listeJeu[1][1] and listeJeu[0][2] == listeJeu[2][0])) {
 		if (listeJeu[1][1] == 'x') {
 			output = "Le joueur 1 gagne";
 		}
 		else if (listeJeu[1][1] == 'o') {
 			output = "Le joueur 2 gagne";
 		}
+
 	}
 	if (output.length() == 0) {
-		cout << "Egalite";
+		output = "Egalite";
 	}
+	cout << output;
 	return 0;
 }
